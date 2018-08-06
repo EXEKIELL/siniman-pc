269932d56862d638805868601504fe156451f325
zhongRui_HE <418351897@qq.com>
2018-08-06 19:50:20 +0800

    '3'

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -17,7 +17,7 @@ if(store.state.login.sign_1 == '' && !localStorage.getItem('sign_1')){
   sign_1 = store.state.login.sign_1 || JSON.parse(localStorage.getItem('sign_1'));
 }
 // console.log(sign_1);
-let root = 'http://localhost:8080/share';
+var root='http://120.24.212.12:8080/share/';
 let headers = [
   {'Content-Type':'application/x-www-form-urlencoded'},
   {'Content-Type': 'application/json'},
@@ -26,58 +26,11 @@ let headers = [
 ]
 // console.log(headers)
 function axiosPost(url,index,params,fun) {
-  let time = Date.parse( new Date() ).toString(),
-      sign = md5(md5(store.state.login.token+time));
-  params.time = time;
-  params.sign = sign;
-  // console.log(sign);
-  axios({
-    method:'post',
-    baseURL:root,
-    url:url,
-    headers:headers[index],
-    data:params,
-    transformRequest:[function (data) {
-      if(index==0){
-        Object.keys(data).forEach((key) => {
-          if ((typeof data[key]) === 'object') {
-            data[key] = JSON.stringify(data[key]); // 这里必须使用内置JSON对象转换
-          }
-        })
-        data = qs.stringify(data); // 这里必须使用qs库进行转换
-        return data
-      }else{
-        data = JSON.stringify(data);
-        return data
-      }
-    }]
-  }).then(res=>{
-    if(res.data.code!=null&&res.data.code=='1003'){
-      router.push('/login')
-    }else if(typeof fun=="function"){
-      fun(res)
-    }
-    $('html , body').animate({scrollTop: 0},'fast');
-  })
+
 }
 function axiosGet(url,index,params,fun) {
-  let time = Date.parse( new Date() ).toString(),
-    sign = md5(md5(store.state.login.token+time));
-  params.time = time;
-  params.sign = sign;
-  console.log(params.time);
-  console.log(params.sign);
-  axios({
-    method:'get',
-    baseURL:root,
-    headers:headers[index],
-    url:url,
-    params:params
-  }).then(res=>{
-    if(typeof fun=="function"){
-      fun(res)
-    }
-  })
+
+
 }
 
 export default {
